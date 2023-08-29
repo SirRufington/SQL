caf10d8f1ffc0eb02c73b470b324304507bc1545
SirRufington <119642359+SirRufington@users.noreply.github.com>
2023-08-28 20:12:47 -0500

    Finalize Business Answers: Complete Q&A for Prospective Buyer.sql

--- Movie Rental Analytics/Insights into Movie Rental Company.sql
+++ Movie Rental Analytics/Insights into Movie Rental Company.sql
@@ -7,6 +7,7 @@ He primarily owns restaurants and bars, so he has lots of questions for you abou
 His offer seems very generous, so you are going to entertain his questions.
 */
 
+
 /* 
 1. My partner and I want to come by each of the stores in person and meet the managers. 
 Please send over the managers’ names at each store, with the full address 
@@ -14,26 +15,26 @@ of each property (street address, district, city, and country please).
 */
 
 SELECT
-	staff.first_name AS Manager_first_name,
+    staff.first_name AS Manager_first_name,
     staff.last_name AS Manager_last_name,
     address.address,
     address.district,
     city.city,
     country.country
 FROM store
-	LEFT JOIN staff ON store.manager_staff_id = staff.staff_id
+    LEFT JOIN staff ON store.manager_staff_id = staff.staff_id
     LEFT JOIN address ON store.address_id = address.address_id
     LEFT JOIN city ON address.city_id = city.city_id
     LEFT JOIN country ON city.country_id = country.country_id;
 
 /*
-2.	I would like to get a better understanding of all of the inventory that would come along with the business. 
+2. I would like to get a better understanding of all of the inventory that would come along with the business. 
 Please pull together a list of each inventory item you have stocked, including the store_id number, 
 the inventory_id, the name of the film, the film’s rating, its rental rate and replacement cost. 
 */
 
 SELECT 
-	inventory.store_id,
+    inventory.store_id,
     inventory.inventory_id,
     film.title,
     film.rating,
@@ -43,19 +44,19 @@ FROM inventory
 	LEFT JOIN film ON inventory.film_id = film.film_id;
     
 /* 
-3.	From the same list of films you just pulled, please roll that data up and provide a summary level overview 
+3. From the same list of films you just pulled, please roll that data up and provide a summary level overview 
 of your inventory. We would like to know how many inventory items you have with each rating at each store. 
 */
 
 SELECT 
-	inventory.store_id,
+    inventory.store_id,
     COUNT(inventory.inventory_id) AS Count_of_film,
     film.rating
 FROM inventory
-	LEFT JOIN film 
-		ON inventory.film_id = film.film_id
+    LEFT JOIN film 
+	ON inventory.film_id = film.film_id
 GROUP BY 
-	film.rating,
+    film.rating,
     inventory.store_id;
     
 /* 
@@ -66,30 +67,30 @@ sliced by store and film category.
 */ 
 
 SELECT
-	inventory.store_id,
+    inventory.store_id,
     category.name AS category,
     COUNT(inventory.inventory_id) AS Count_of_films,
     AVG(film.replacement_cost) AS Average_replacemnet_cost,
     SUM(film.replacement_cost) AS Total_replacement_cost
 FROM inventory
     LEFT JOIN film 
-		ON inventory.film_id = film.film_id
+	ON inventory.film_id = film.film_id
     LEFT JOIN film_category 
-		ON film.film_id = film_category.film_id
+	ON film.film_id = film_category.film_id
     LEFT JOIN category 
-		ON film_category.category_id = category.category_id
+	ON film_category.category_id = category.category_id
 GROUP BY 
-	category.name,
+    category.name,
     inventory.store_id;
     
 /*
-5.	We want to make sure you folks have a good handle on who your customers are. Please provide a list 
+5. We want to make sure you folks have a good handle on who your customers are. Please provide a list 
 of all customer names, which store they go to, whether or not they are currently active, 
 and their full addresses – street address, city, and country. 
 */
 
 SELECT
-	customer.store_id,
+    customer.store_id,
     customer.first_name,
     customer.last_name,
     customer.active,
@@ -102,22 +103,22 @@ FROM customer
 	LEFT JOIN country ON city.country_id = country.country_id;
     
     /*
-6.	We would like to understand how much your customers are spending with you, and also to know 
+6. We would like to understand how much your customers are spending with you, and also to know 
 who your most valuable customers are. Please pull together a list of customer names, their total 
 lifetime rentals, and the sum of all payments you have collected from them. It would be great to 
 see this ordered on total lifetime value, with the most valuable customers at the top of the list. 
 */
 
 SELECT
-	customer.first_name,
+    customer.first_name,
     customer.last_name,
     COUNT(rental.rental_id) AS total_rentals,
     SUM(payment.amount) AS total_payment_amount
 FROM customer
-	LEFT JOIN rental ON customer.customer_id = rental.customer_id
+    LEFT JOIN rental ON customer.customer_id = rental.customer_id
     LEFT JOIN payment ON rental.rental_id = payment.rental_id
 GROUP BY
-	customer.first_name,
+    customer.first_name,
     customer.last_name
 ORDER BY SUM(payment.amount) DESC;
 
@@ -129,7 +130,7 @@ it would be good to include which company they work with.
 */
 
 SELECT
-	'Advisor' AS TYPE,
+    'Advisor' AS TYPE,
     first_name,
     last_name,
     NULL
@@ -138,7 +139,7 @@ FROM advisor
 UNION
 
 Select
-	'investor' AS TYPE,
+    'investor' AS TYPE,
     first_name,
     last_name,
     company_name
@@ -152,12 +153,12 @@ Finally, how about actors with just one award?
 */
 
 SELECT
-	CASE
-		WHEN actor_award.awards = 'Emmy, Oscar, Tony ' THEN '3 Awards'
+    CASE
+	WHEN actor_award.awards = 'Emmy, Oscar, Tony ' THEN '3 Awards'
         WHEN actor_award.awards IN ('Emmy, Oscar','Emmy, Tony', 'Oscar, Tony') THEN '2 Awards'
         ELSE '1 award'
 	END AS Num_of_Awards,
     AVG(CASE WHEN actor_award.actor_id IS NULL THEN 0 ELSE 1 END) * 100 AS pct_w_one_film
 FROM actor_award
 GROUP BY 
-	Num_of_Awards
+    Num_of_Awards
